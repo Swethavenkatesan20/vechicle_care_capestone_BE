dfb52611dcc7ad138abbc3bd150accd4cbbbc12f
Swethavenkatesan20 <157340837+Swethavenkatesan20@users.noreply.github.com>
2024-05-04 13:58:33 +0530

    Update app.js
    changed cors url

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ const app=express();
 
 //enable cors request
 app.use(cors({
-    origin:'http://localhost:5173',  //allow all origin
+    origin:'*',  //allow all origin
     credentials:true
 }))
 
